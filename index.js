69b7d5bb82748773fa3a078449798e86653f0506
Harsh-Kant <harshkant2020@gmail.com>
2025-04-04 03:19:22 +0530

    beginner sum functionality

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express()
+app.use(cors())
+
+app.get("/sum", (req, res)=>{
+    const a = parseInt(req.query.a)
+    const b = parseInt(req.query.b)
+    const sum = a+b
+    res.send(sum.toString())
+})
+
+app.listen(8000, ()=>{
+    console.log("Server is running on port 8000")
+})
