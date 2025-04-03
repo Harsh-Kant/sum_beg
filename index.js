e14c87948bdfd5d9249f734c1e25a115b9de8e79
Harsh-Kant <harshkant2020@gmail.com>
2025-04-04 04:02:09 +0530

    add multiply and interest endpoints

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ app.get("/sum", (req, res)=>{
     res.send(sum.toString())
 })
 
+app.get("/multiply", (req, res)=>{
+    const a = parseInt(req.query.a)
+    const b = parseInt(req.query.b)
+    const sum = a*b
+    res.send(sum.toString())
+})
+
+app.get("/interest", (req, res)=>{
+    const a = parseInt(req.query.a)
+    const b = parseInt(req.query.b)
+    const c = parseInt(req.query.c)
+    const interest = (a*b*c)/100
+    res.send(interest.toString())
+})
+
 app.listen(8000, ()=>{
     console.log("Server is running on port 8000")
 })
